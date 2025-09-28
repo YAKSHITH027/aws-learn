c96c96288122a11bdd02fe64bf2dcb1665255e24
YAKSHITH027 <yakshith027@gmail.com>
2025-09-28 23:24:31 +0530

    place updated

--- client/src/state/index.ts
+++ client/src/state/index.ts
@@ -20,7 +20,7 @@ interface InitialStateTypes {
 
 export const initialState: InitialStateTypes = {
   filters: {
-    location: "Los Angeles",
+    location: "Udupi",
     beds: "any",
     baths: "any",
     propertyType: "any",
@@ -28,7 +28,7 @@ export const initialState: InitialStateTypes = {
     availableFrom: "any",
     priceRange: [null, null],
     squareFeet: [null, null],
-    coordinates: [-118.25, 34.05],
+    coordinates: [74.74825, 13.340317],
   },
   isFiltersFullOpen: false,
   viewMode: "grid",
