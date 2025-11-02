3301b579c08db5697a19d4ac1e9d37fcdc62c2aa
YAKSHITH027 <yakshith027@gmail.com>
2025-11-03 00:17:37 +0530

    delete api for temp

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ import managerRoutes from "./routes/managerRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
 import leaseRoutes from "./routes/leaseRoutes";
 import applicationRoutes from "./routes/applicationRoutes";
+import { deleteAllProperties } from "./controllers/propertyControllers";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -33,6 +34,7 @@ app.use("/properties", propertyRoutes);
 app.use("/leases", leaseRoutes);
 app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 app.use("/managers", authMiddleware(["manager"]), managerRoutes);
+app.use("/deleteall77", deleteAllProperties);
 
 /* SERVER */
 const port = Number(process.env.PORT) || 3002;
